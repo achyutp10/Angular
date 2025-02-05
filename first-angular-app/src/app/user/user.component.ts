99813d2667df0aa97630793e4cd88e4c254e7c45
Achyut <achyutp10@gmail.com>
2025-02-05 12:49:58 +0545

    output fn

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 // import { Component, computed, signal } from '@angular/core';
-import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
 // import { DUMMY_USERS } from '../dummy-users';
 
@@ -32,7 +32,8 @@ export class UserComponent {
   @Input({required:true}) id!: string;
   @Input({required:true}) avatar!: string;
   @Input({required:true}) name!: string;
-  @Output() select = new EventEmitter();
+  // @Output() select = new EventEmitter();
+  select = output<string>();
 
 //   avatar = input.required<string>();
 //   name = input.required<string>();
