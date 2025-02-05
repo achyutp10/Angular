f554d275c849c8344d6a7e7ab150ce910352fd72
Achyut <achyutp10@gmail.com>
2025-02-05 12:08:50 +0545

    user signals input

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,8 +1,9 @@
-import { Component, computed, signal } from '@angular/core';
+// import { Component, computed, signal } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+// import { DUMMY_USERS } from '../dummy-users';
 
-const randonIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+// const randonIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -11,17 +12,39 @@ const randonIndex = Math.floor(Math.random()*DUMMY_USERS.length)
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
+// export class UserComponent {
+//   // selectedUser = DUMMY_USERS[randonIndex];
+//   // selectedUser = signal(DUMMY_USERS[randonIndex]);
+//   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+//   // get imagePath() {
+//   //   return 'assets/users/' + this.selectedUser.avatar
+//   // }
+//   onSelectUser() {
+//     // const randonIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+//     // this.selectedUser.set(DUMMY_USERS[randonIndex])
+//     // this.selectedUser = DUMMY_USERS[randonIndex];
+//   };
+// };
+
 export class UserComponent {
-  // selectedUser = DUMMY_USERS[randonIndex];
-  selectedUser = signal(DUMMY_USERS[randonIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
+  // @Input({required:true}) avatar!: string;
+  // @Input({required:true}) name!: string;
+
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    'assets/users/' + this.avatar();
+});
 
   // get imagePath() {
-  //   return 'assets/users/' + this.selectedUser.avatar
+  //     return 'assets/users/' + this.avatar
   // }
+
+
   onSelectUser() {
-    const randonIndex = Math.floor(Math.random()*DUMMY_USERS.length)
-    this.selectedUser.set(DUMMY_USERS[randonIndex])
-    // this.selectedUser = DUMMY_USERS[randonIndex];
+
   };
 };
