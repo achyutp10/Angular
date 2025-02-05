fd985a426574584d5cd527997d1770166b0203b7
Achyut <achyutp10@gmail.com>
2025-02-05 12:45:34 +0545

    output and emmiting data

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 // import { Component, computed, signal } from '@angular/core';
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
 
 // import { DUMMY_USERS } from '../dummy-users';
 
@@ -29,22 +29,24 @@ import { Component, computed, Input, input } from '@angular/core';
 
 export class UserComponent {
 
-  // @Input({required:true}) avatar!: string;
-  // @Input({required:true}) name!: string;
+  @Input({required:true}) id!: string;
+  @Input({required:true}) avatar!: string;
+  @Input({required:true}) name!: string;
+  @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+//   avatar = input.required<string>();
+//   name = input.required<string>();
 
-  imagePath = computed(() => {
-    'assets/users/' + this.avatar();
-});
+//   imagePath = computed(() => {
+//     'assets/users/' + this.avatar();
+// });
 
-  // get imagePath() {
-  //     return 'assets/users/' + this.avatar
-  // }
+  get imagePath() {
+      return 'assets/users/' + this.avatar
+  }
 
 
   onSelectUser() {
-
+    this.select.emit(this.id);
   };
 };
