924e5b602f8d12e7f9552fc98e435a7aa9cec9da
Achyut <achyutp10@gmail.com>
2025-02-08 21:20:56 +0545

    getting inputted data with result

--- practice/src/app/app.component.ts
+++ practice/src/app/app.component.ts
@@ -1,21 +1,27 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  onCalculateInvestmentResults(
-    data: {
-    initialInvestment: number,
-    duration: number,
-    expectedReturn: number,
-    annualInvestment: number
-  }) {
+
+  resultsData?: {
+    year: number;
+    interest: number;
+    valueEndOfYear: number;
+    annualInvestment: number;
+    totalInterest: number;
+    totalAmountInvested: number;
+  }[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
     const {initialInvestment,annualInvestment,expectedReturn,duration} = data;
     const annualData = [];
     let investmentValue = initialInvestment;
@@ -36,8 +42,8 @@ export class AppComponent {
       });
     }
 
-    // return annualData;
-    console.log(annualData);
+    this.resultsData = annualData;
+    // console.log(annualData);
 
   }
 }
---
+++ practice/src/app/investment-input.model.ts
@@ -0,0 +1,14 @@
+export interface InvestmentInput {
+  initialInvestment: number,
+  duration: number,
+  expectedReturn: number,
+  annualInvestment: number
+}
+
+// type InvestInput = {
+//   initialInvestment: number,
+//   duration: number,
+//   expectedReturn: number,
+//   annualInvestment: number
+// }
+
