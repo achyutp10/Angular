98b253cdac6b1fcbe0f032cae8acbdf468558684
Achyut <achyutp10@gmail.com>
2025-02-12 20:01:21 +0545

    attribute directive to change element behaviour

--- directives/src/app/safe-link.directive.ts
+++ directives/src/app/safe-link.directive.ts
@@ -2,10 +2,20 @@ import { Directive } from "@angular/core";
 
 @Directive({
   selector: 'a[appSafeLink]',
-  standalone: true
+  standalone: true,
+  host: {
+    '(click)': 'onConformLeavePage($event)'
+  }
 })
 export class SafeLinkDirective {
   constructor() {
     console.log("Safe link DIrective is active!");
+  }
+  onConformLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do You Want To Leave the App?')
+    if (wantsToLeave) {
+      return;
+    }
+    event?.preventDefault();
   }
 }
