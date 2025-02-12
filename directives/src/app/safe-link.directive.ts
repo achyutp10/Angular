74e70bf60e7c6b9147464d0a3111d3a07fec66d6
Achyut <achyutp10@gmail.com>
2025-02-12 20:47:59 +0545

    host directive

--- directives/src/app/safe-link.directive.ts
+++ directives/src/app/safe-link.directive.ts
@@ -1,11 +1,13 @@
 import { Directive, ElementRef, inject, input } from "@angular/core";
+import { LogDirective } from "./log.directive";
 
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
   host: {
     '(click)': 'onConformLeavePage($event)'
-  }
+  },
+  hostDirectives: [LogDirective]
 })
 export class SafeLinkDirective {
   queryParam = input('myapp',{alias:'appSafeLink'});
