95fadf0cf03d7ad060d257122c1a380eefefb5a0
Achyut <achyutp10@gmail.com>
2025-02-13 19:34:26 +0545

    Angular DI mechanism

---
+++ service-DI/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
+
+@Component({
+  selector: 'app-new-task',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.css',
+})
+export class NewTaskComponent {
+  private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
+
+  // private tasksService: TasksService;
+
+  // constructor() {
+  //   this.tasksService = new TasksService();
+  // }
+
+  // constructor(tService: TasksService) {
+  //   this.tasksService = tService;
+  // }
+
+  constructor(private tasksService: TasksService) {
+  }
+
+  onAddTask(title: string, description: string) {
+    this.tasksService.addTask({title, description})
+    this.formEl()?.nativeElement.reset();
+  }
+}
