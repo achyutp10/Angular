8d6de31093548229e89cb7154cb32e26cd797d4a
Achyut <achyutp10@gmail.com>
2025-02-15 14:41:39 +0545

    DELETE Request

--- Http-Response/src/app/places/user-places/user-places.component.ts
+++ Http-Response/src/app/places/user-places/user-places.component.ts
@@ -43,4 +43,10 @@ export class UserPlacesComponent implements OnInit {
         subscription.unsubscribe();
       });
     }
+    onRemovePlace(place: Place) {
+      const subscription = this.placesService.removeUserPlace(place).subscribe();
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      });
+    }
 }
