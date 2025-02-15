261e98c7de3e2697aff730171afe58355651b681
Achyut <achyutp10@gmail.com>
2025-02-15 17:32:49 +0545

    async validators

--- reactive-forms/src/app/auth/login/login.component.ts
+++ reactive-forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -7,6 +8,12 @@ function mustContainQuestionMark(control: AbstractControl) {
   }
 
   return {doesNotContainQuestionMark: true}
+}
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !=='test@gmail.com') {
+    return of(null)
+  }
+  return of({notUnique:true})
 }
 @Component({
   selector: 'app-login',
@@ -20,9 +27,10 @@ export class LoginComponent {
   form = new FormGroup({
     email: new FormControl('',{
       validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
+      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
     }),
   });
 
