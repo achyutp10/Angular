8a233ba6e2dcaeca9fdb17d3c3f0f240cb7d8370
Achyut <achyutp10@gmail.com>
2025-02-16 18:26:59 +0545

    second effect

--- NgRx-standalone/src/app/store/counter.reducer.ts
+++ NgRx-standalone/src/app/store/counter.reducer.ts
@@ -1,5 +1,5 @@
 import { Action, createReducer, on } from "@ngrx/store";
-import { decrement, increment } from "./counter.actions";
+import { decrement, increment, set } from "./counter.actions";
 // import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
 
 const initialState = 0;
@@ -8,6 +8,7 @@ export const counterReducer = createReducer(
    initialState,
   on(increment, (state, action) => state + action.value),
   on(decrement, (state, action) => state - action.value),
+  on(set, (state, action) => action.value),
 );
 
 // export function counterReducer(state = initialState, action: CounterActions | Action) {
