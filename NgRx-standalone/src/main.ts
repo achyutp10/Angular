a01c478934a84fdaeb698586c8b6fab0b196135c
Achyut <achyutp10@gmail.com>
2025-02-16 18:08:08 +0545

    effect

--- NgRx-standalone/src/main.ts
+++ NgRx-standalone/src/main.ts
@@ -3,10 +3,10 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './app/store/counter.reducer';
+import { provideEffects } from '@ngrx/effects';
 
 bootstrapApplication(AppComponent, {
     providers: [provideStore({
-      counter: counterReducer,
-
-    })]
+        counter: counterReducer,
+    }), provideEffects()]
 });
